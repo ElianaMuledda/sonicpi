3f40ad96f4096e8fdc3a061b3964441426b414d7
ElianaMuledda <45307977+ElianaMuledda@users.noreply.github.com>
2018-11-30 18:50:28 +0100

    Update Lezione1.rb

--- Lezione1.rb
+++ Lezione1.rb
@@ -1,4 +1,18 @@
 live_loop :flibble do
   sample :bd_haus, rate: 1
-  sleep 0.5
+  sleep 0.5sample :ambi_choir, rate: 0.3
+sample :bd_haus, rate: 0.6
+sleep 0.7
 end
+live_loop :guit do
+  with_fx :echo, mix: 0.5, phase: 0.2 do
+    sample :guit_em9, rate: 1
+  end
+  sample :guit_em9, rate: 1
+  sleep 8
+end
+live_loop :boom do
+  with_fx :reverb, room: 0.4 do
+    sample :bd_boom, amp: 5, rate: 0.5
+    sleep 4
+  end
